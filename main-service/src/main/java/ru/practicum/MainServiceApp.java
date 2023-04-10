d76b5e61f6e07cac6bc2964019222ec167475e09
MrOlegB <legan26021991@yandex.ru>
2023-04-10 15:50:04 +0300

    refactor: собрал Структуру модулей и зависимостей

--- main-service/src/main/java/ru/practicum/MainServiceApp.java
+++ main-service/src/main/java/ru/practicum/MainServiceApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StatServiceStatServer {
+public class MainServiceApp {
     public static void main(String[] args) {
-        SpringApplication.run(StatServiceStatServer.class, args);
+        SpringApplication.run(MainServiceApp.class, args);
     }
 }
